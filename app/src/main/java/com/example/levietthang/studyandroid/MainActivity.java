ff9d4d57a3153cf31e0a3d76b1d4b97e73057809
thanglv1302 <levietthang1302@gmail.com>
2019-10-06 22:06:01 +0700

    implement call phone for OS android

--- app/src/main/java/com/example/levietthang/studyandroid/MainActivity.java
+++ app/src/main/java/com/example/levietthang/studyandroid/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.levietthang.studyandroid;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.buttonCall);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:0363573172"));
+
+                if(ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
     }
 }
