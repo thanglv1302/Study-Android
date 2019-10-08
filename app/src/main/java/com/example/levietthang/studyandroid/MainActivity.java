ba326cc923c3940e1b64f64014ec17a8270210ee
thanglv1302 <levietthang1302@gmail.com>
2019-10-09 00:43:04 +0700

    Implement manager music

--- app/src/main/java/com/example/levietthang/studyandroid/MainActivity.java
+++ app/src/main/java/com/example/levietthang/studyandroid/MainActivity.java
@@ -5,23 +5,31 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import java.lang.reflect.Field;
 
 import static android.media.AudioManager.RINGER_MODE_NORMAL;
 import static android.media.AudioManager.RINGER_MODE_SILENT;
 import static android.media.AudioManager.RINGER_MODE_VIBRATE;
+import static android.media.AudioManager.STREAM_MUSIC;
 
 public class MainActivity extends Activity {
     private static final int ON_DO_NOT_DISTURB_CALLBACK_CODE = 0;
-    Button mode, ring, vibrate, silent;
+    private Button mode, ring, vibrate, silent, mute, maxVolume, volUp, volDown;
+    private ProgressBar volumeChange;
     private AudioManager myAudioManager;
+    private int volumeMax, volumeMin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +42,17 @@ public class MainActivity extends Activity {
         ring = (Button) findViewById(R.id.btnRing);
         vibrate = (Button) findViewById(R.id.btnVib);
         silent = (Button) findViewById(R.id.btnSilent);
+        mute = (Button) findViewById(R.id.btnMute);
+        maxVolume = (Button) findViewById(R.id.btnMaxVolume);
+        volumeChange = (ProgressBar) findViewById(R.id.volumeBar);
+        volUp = (Button) findViewById(R.id.btnVolUp);
+        volDown = (Button) findViewById(R.id.btnVolDown);
+
         myAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        volumeMax = myAudioManager.getStreamMaxVolume(STREAM_MUSIC);
+        volumeMin = myAudioManager.getStreamMinVolume(STREAM_MUSIC);
+        volumeChange.setMax(volumeMax);
+        volumeChange.setProgress(myAudioManager.getStreamVolume(STREAM_MUSIC));
         ring.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,6 +93,54 @@ public class MainActivity extends Activity {
                 }
             }
         });
+
+        mute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestMuteSound();
+            }
+        });
+
+        maxVolume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestMaxVolume();
+            }
+        });
+
+        volUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currVol = volumeChange.getProgress();
+                if (currVol < volumeMax) {
+                    currVol += volumeMax/(volumeChange.getMax());
+                    volumeChange.setProgress(currVol);
+                    myAudioManager.setStreamVolume(STREAM_MUSIC, currVol, 0);
+                }
+            }
+        });
+
+        volDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currVol = volumeChange.getProgress();
+                if (currVol > volumeMin) {
+                    currVol -= volumeMax/(volumeChange.getMax());
+                    volumeChange.setProgress(currVol);
+                    myAudioManager.setStreamVolume(STREAM_MUSIC,currVol,0);
+                }
+            }
+        });
+    }
+
+    private void requestMuteSound(){
+        volumeChange.setProgress(volumeMin);
+        myAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC,volumeMin,0);
+    }
+
+    private void requestMaxVolume(){
+        volumeChange.setProgress(volumeMax);
+        myAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC,volumeMax, 0);
     }
 
     private void requestMutePermissions() {
